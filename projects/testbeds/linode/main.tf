c8a136950de91f6fda75895c7f03bf52495e43bf
Mitul Tandon <mtandon@vmware.com>
2024-10-18 12:16:57 +0530

    Update linode testbed

--- projects/testbeds/linode/main.tf
+++ projects/testbeds/linode/main.tf
@@ -17,3 +17,14 @@ terraform {
 provider "linode" {
   token = var.linode_token
 }
+
+resource "linode_instance" "k8s-express--master" {
+  label           = "k8s-express-master"
+  image           = "linode/ubuntu22.04"
+  region          = "us-central"
+  type            = "g6-standard-1"
+  authorized_keys = var.ssh_public_keys
+  root_pass       = var.linode_vm_password
+
+  tags       = ["k8s-express"]
+}
--- projects/testbeds/linode/variables.tf
+++ projects/testbeds/linode/variables.tf
@@ -1,5 +1,17 @@
 variable "linode_token" {
   description = "Linode API token"
   type        = string
-  default = ""
+  default     = ""
+}
+
+variable "ssh_public_keys" {
+  description = "SSH Public Key"
+  type        = list
+  default     = [""]
+}
+
+variable "linode_vm_password" {
+  description = "Linode VM Password"
+  type        = string
+  default     = ""
 }
